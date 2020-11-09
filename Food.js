82c9df2fa94e192635343da7c9e4b14305fa7c76
tejaswanama111 <71207920+tejaswanama111@users.noreply.github.com>
2020-11-09 09:20:09 -0800

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,27 +1,38 @@
 class Food {
-    constructor(){
-      this.foodStock = foodStock;
-      this.lastFed = lastFed;
-      this.image = loadImage("images/milk.png");
-    }
+  constructor(){
+  this.foodStock = database.ref('Food');
+  this.image=loadImage('images/milk.png');
+  }
 
-    display(){
-        var x=80;
-        var y=100;
-        imageMode(CENTER);
-        image(this.image, 50, 180, 70, 70);
+ updateFoodStock(foodStock){
+  this.foodStock=foodStock;
+ }
 
-        for(var i=50; i <=500; i=i+50){
-          milk = createSprite(i, 180);
-          milk.addImage("milk", milkImg);
-          milk.scale= 0.1;
+ deductFood(){
+   if(this.foodStock>0){
+    this.foodStock=this.foodStock-1;
+   }
+  }
+
+  getFoodStock(){
+    return this.foodStock;
+  }
+
+  display(){
+    var x=80,y=170;
+    
+    imageMode(CENTER);
+ 
+    
+    if(this.foodStock!=0){
+      for(var i=0;i<this.foodStock;i++){
+        if(i%10===0){
+          x=80;
+          y=y+50;
         }
-
-        for(var i=50; i <=500; i=i+50){
-          milk = createSprite(i, 280);
-          milk.addImage("milk", milkImg);
-          milk.scale= 0.1;
-        }
-          
+        image(this.image,x,y,50,50);
+        x=x+30;
       }
+    }
+  }
 }
